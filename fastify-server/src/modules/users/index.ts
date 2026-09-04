1fb92f87fe038c44ca4b6e66d866cc98ef89b65b
Rajat Verma <cserajatverma@gmail.com>
2026-09-04 23:29:54 +0530

    feat(users): export user module

---
+++ fastify-server/src/modules/users/index.ts
@@ -0,0 +1,7 @@
+export * from './types/user.types';
+export * from './errors/user.errors';
+export * from './schemas/user.schema';
+export * from './repositories/user.repository';
+export * from './services/user.service';
+export * from './controllers/user.controller';
+export * from './routes/user.routes';
