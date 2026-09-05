edc0a48af8f80cfea8910a72447aa769e4d564d7
Rajat Verma <cserajatverma@gmail.com>
2026-09-05 12:32:53 +0530

    feat(goals): export goals module

---
+++ fastify-server/src/modules/goals/index.ts
@@ -0,0 +1,7 @@
+export * from './types/goal.types';
+export * from './errors/goal.errors';
+export * from './schemas/goal.schema';
+export * from './repositories/goal.repository';
+export * from './services/goal.service';
+export * from './controllers/goal.controller';
+export * from './routes/goal.routes';
