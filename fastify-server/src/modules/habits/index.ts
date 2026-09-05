1cad74dcc6ec899efd97949dd23b835838c56194
Rajat Verma <cserajatverma@gmail.com>
2026-09-05 12:32:54 +0530

    feat(habits): export habits module

---
+++ fastify-server/src/modules/habits/index.ts
@@ -0,0 +1,6 @@
+export * from './types/habit.types';
+export * from './schemas/habit.schema';
+export * from './repositories/habit.repository';
+export * from './services/habit.service';
+export * from './controllers/habit.controller';
+export * from './routes/habit.routes';
