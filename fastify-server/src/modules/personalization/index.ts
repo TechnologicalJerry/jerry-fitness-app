a9978e0a341896e4eb72ce5c2780c37eedfa70de
Rajat Verma <cserajatverma@gmail.com>
2026-09-05 12:32:56 +0530

    feat(personalization): export personalization module

---
+++ fastify-server/src/modules/personalization/index.ts
@@ -0,0 +1,6 @@
+export * from './types/personalization.types';
+export * from './schemas/personalization.schema';
+export * from './repositories/personalization.repository';
+export * from './services/personalization.service';
+export * from './controllers/personalization.controller';
+export * from './routes/personalization.routes';
