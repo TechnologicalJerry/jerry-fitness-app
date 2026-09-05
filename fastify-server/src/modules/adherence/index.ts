ae607e351fef26350e927896f714f273e7378b52
Rajat Verma <cserajatverma@gmail.com>
2026-09-05 12:32:51 +0530

    feat(adherence): export adherence module

---
+++ fastify-server/src/modules/adherence/index.ts
@@ -0,0 +1,6 @@
+export * from './types/adherence.types';
+export * from './schemas/adherence.schema';
+export * from './repositories/adherence.repository';
+export * from './services/adherence.service';
+export * from './controllers/adherence.controller';
+export * from './routes/adherence.routes';
