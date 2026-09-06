c54c8fb005ab04f6ca5c85e76d8d166ebb5054b8
Rajat Verma <cserajatverma@gmail.com>
2026-09-06 18:04:26 +0530

    feat(training-load): export training load module

---
+++ fastify-server/src/modules/training-load/index.ts
@@ -0,0 +1,6 @@
+export * from './types/training-load.types';
+export * from './schemas/training-load.schema';
+export * from './repositories/training-load.repository';
+export * from './services/training-load.service';
+export * from './controllers/training-load.controller';
+export * from './routes/training-load.routes';
