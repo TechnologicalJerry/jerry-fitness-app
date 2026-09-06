23d2936ab7f1b088a55f7c0d67bdc0267534d51c
Rajat Verma <cserajatverma@gmail.com>
2026-09-06 18:04:25 +0530

    feat(recovery): export recovery module

---
+++ fastify-server/src/modules/recovery/index.ts
@@ -0,0 +1,6 @@
+export * from './types/recovery.types';
+export * from './schemas/recovery.schema';
+export * from './repositories/recovery.repository';
+export * from './services/recovery.service';
+export * from './controllers/recovery.controller';
+export * from './routes/recovery.routes';
