d277d329ab94ed4d802e39562dfec61945442e4b
Rajat Verma <cserajatverma@gmail.com>
2026-09-08 20:21:39 +0530

    feat(app): register application updates

--- fastify-server/src/app.ts
+++ fastify-server/src/app.ts
@@ -1,4 +1,5 @@
 import Fastify, { FastifyInstance } from 'fastify';
+import fastifyWebsocket from '@fastify/websocket';
 import { requestIdMiddleware } from './common/middleware/request-id';
 import corsPlugin from './plugins/cors';
 import helmetPlugin from './plugins/helmet';
@@ -7,6 +8,7 @@ import sensiblePlugin from './plugins/sensible';
 import swaggerPlugin from './plugins/swagger';
 import prismaPlugin from './plugins/prisma';
 import redisPlugin from './plugins/redis';
+import authPlugin from './plugins/auth';
 import errorHandlerPlugin from './plugins/error-handler';
 import { appRoutes } from './routes';
 import { logger } from './observability/logger';
@@ -21,6 +23,9 @@ export function buildApp(): FastifyInstance {
   // Request ID middleware hook
   app.addHook('onRequest', requestIdMiddleware);
 
+  // WebSocket Plugin
+  app.register(fastifyWebsocket);
+
   // Core Security & Framework Plugins
   app.register(corsPlugin);
   app.register(helmetPlugin);
@@ -31,6 +36,7 @@ export function buildApp(): FastifyInstance {
   // Infrastructure Lifecycle Plugins
   app.register(prismaPlugin);
   app.register(redisPlugin);
+  app.register(authPlugin);
 
   // Centralized Error Handler Plugin
   app.register(errorHandlerPlugin);
