17bd5e6b191501e220615608a97c154c882f7c7b
Rajat Verma <cserajatverma@gmail.com>
2026-09-11 19:29:29 +0530

    chore(vue): update Nuxt configuration

--- vue-client/nuxt.config.ts
+++ vue-client/nuxt.config.ts
@@ -3,8 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3000/api/v1',
+    },
+  },
+
   modules: [
-    '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
