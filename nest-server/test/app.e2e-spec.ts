520d77c87622425a4782d593570f533fdeca6989
Rajat Verma <cserajatverma@gmail.com>
2026-09-11 19:29:28 +0530

    test(nest): update end-to-end app tests

--- nest-server/test/app.e2e-spec.ts
+++ nest-server/test/app.e2e-spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from './../src/app.module.js';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
@@ -21,5 +21,9 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 });
